05472e152f02fe2dcef866a6500ec7adf9001afc
Mark Marilag <mark.marilag27@gmail.com>
2023-10-24 01:42:16 +0800

    fix login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends Controller
 {
@@ -16,6 +20,20 @@ class LoginController extends Controller
         return view('pages.auth.login');
     }
 
-    public function store(Request $request)
-    {}
+    public function store(LoginRequest $request)
+    {
+        /** @var array $payload */
+        $payload = $request->validated();
+
+        /** @var User $user */
+        $user = User::query()->where('email', $payload['email'])->first();
+
+        if (!Hash::check($payload['password'], $user->password)) {
+            return back()->withErrors(['password' => __('auth.password')]);
+        }
+
+        Auth::loginUsingId($user->id);
+
+        return redirect('dashboard');
+    }
 }
